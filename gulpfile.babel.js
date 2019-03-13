e0534ab97fc4fadda42050519fc71cab3fe74530
Ismael Terreno <iterreno@zigatta.com>
2019-03-13 16:30:34 -0300

    Desktop markup and base mobile detection.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -120,7 +120,7 @@ gulp.task('copy:license', () =>
 gulp.task('copy:main.css', () => {
   const banner = `/*! HTML5 Boilerplate v${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n\n`;
 
-  gulp.src(`node_modules/main.css/dist/main.css`)
+  gulp.src(`./src/css/main.css`)
     .pipe(plugins().header(banner))
     .pipe(plugins().autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9', '> 1%'],
